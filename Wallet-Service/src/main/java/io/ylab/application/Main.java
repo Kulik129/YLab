499d100f7241b80b594f6e8196905104a77f6297
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-10-16 21:01:53 +0300

    liquibase

--- Wallet-Service/src/main/java/io/ylab/application/Main.java
+++ Wallet-Service/src/main/java/io/ylab/application/Main.java
@@ -3,6 +3,14 @@ package io.ylab.application;
 import io.ylab.domain.service.UserService;
 import io.ylab.infrastructure.in.InputDataService;
 import io.ylab.infrastructure.repository.UserRepository;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  * Точка входа в приложение.
@@ -10,6 +18,8 @@ import io.ylab.infrastructure.repository.UserRepository;
 
 public class Main {
     public static void main(String[] args) {
+        // Запускаем миграции
+        runDatabaseMigrations();
         // Создаем репозиторий пользователей
         UserRepository userRepository = new UserRepository();
         // Создаем сервис пользователей, внедряя в него репозиторий
@@ -18,5 +28,24 @@ public class Main {
         InputDataService inputDataService = new InputDataService(userService);
 
         inputDataService.start();
+    }
+
+    /**
+     * Запуск миграций
+     */
+    private static void runDatabaseMigrations() {
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/ylab",
+                    "user",
+                    "12345678"
+            );
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Liquibase liquibase = new Liquibase("db/changelog/chenchelog.xml", new ClassLoaderResourceAccessor(), database);
+            liquibase.update();
+            System.out.println("Миграции успешно выполнены!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
